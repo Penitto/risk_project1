70261b2595dd96c9c3b89226976626d238fe6e1c
Anton <penitto02@gmail.com>
2020-02-14 13:15:17 +0300

    act_risks, act_instruments

--- model.py
+++ model.py
@@ -121,7 +121,50 @@ def get_data():
 
     zero_bond = './zerobond.csv'
 
+    act_instruments = pd.read_csv(shares[0], index_col='<DATE>').drop(['<TICKER>', '<PER>', '<TIME>', '<HIGH>', '<LOW>', '<VOL>', '<OPEN>'], axis=1)
+    act_instruments.index = pd.to_datetime(act_instruments.index)
+    k = 1
+    for i in shares[1:]:
+        tmp = pd.read_csv(i, index_col='<DATE>') \
+                .drop(['<TICKER>', '<PER>', '<TIME>', '<HIGH>', '<LOW>', '<VOL>', '<OPEN>'], axis=1) \
+                .rename(columns={'<CLOSE>': shares_name[k]})
+        tmp.index = pd.to_datetime(tmp.index)
+        act_instruments = act_instruments.join(tmp, how='left')
+        k += 1
+        
+    k = 0
+    for i in bonds:
+        tmp = pd.read_csv(i, index_col='<DATE>') \
+                .drop(['<TICKER>', '<PER>', '<TIME>', '<HIGH>', '<LOW>', '<VOL>', '<OPEN>'], axis=1) \
+                .rename(columns={'<CLOSE>' : bonds_name[k]})
+        tmp.index = pd.to_datetime(tmp.index)
+        act_instruments = act_instruments.join(tmp, how='left')
+        k += 1
 
+    act_instruments = act_instruments.rename(columns={'<CLOSE>' : shares_name[0]})
+        
+    act_risks = pd.read_csv(currencies[0], index_col='<DATE>').drop(['<TICKER>', '<PER>', '<TIME>', '<HIGH>', '<LOW>', '<VOL>', '<OPEN>'], axis=1)
+    act_risks.index = pd.to_datetime(act_risks.index) 
+
+    tmp = pd.read_csv(i, index_col='Date') \
+            .drop(['Open', 'High', 'Low', 'Change %'], axis=1) \
+            .rename(columns={'Price' : currencies_name[k]})
+    tmp.index = pd.to_datetime(tmp.index)
+    act_risks = act_risks.join(tmp, how='left')
+    
+    k = 0
+    for i in indexes:
+        tmp = pd.read_csv(i, index_col='<DATE>') \
+                .drop(['<TICKER>', '<PER>', '<TIME>', '<HIGH>', '<LOW>', '<VOL>', '<OPEN>'], axis=1) \
+                .rename(columns={'<CLOSE>' : indexes_name[k]})
+        tmp.index = pd.to_datetime(tmp.index)
+        act_risks = act_risks.join(tmp, how='left')
+        k += 1
+
+    zero_bond_df = pd.read_csv(zero_bond, sep=';', index_col='Date')
+    zero_bond_df.index = pd.to_datetime(zero_bond_df.index)
+    act_risks = act_risks.join(zero_bond_df, how='left')
+    act_risks = act_risks.fillna(act_risks.mean(axis=0))
 
     r_risks = pd.DataFrame() # Риск-факторы, посчитанные в арифметических процентах
     r_instruments = pd.DataFrame() # доходность инструментов, посчитанная в арифметических процентах
